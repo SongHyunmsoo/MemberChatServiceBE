6e167953bc719d22d654d876b503cf5c491187e5
SongHyunmsoo <y85858@naver.com>
2024-02-08 10:54:56 +0900

    프로젝트 시작

---
+++ src/main/java/Member_chat_service/MemberChatServiceApplication.java
@@ -0,0 +1,13 @@
+package Member_chat_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberChatServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Member_chat_service/MemberChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Member_chat_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
