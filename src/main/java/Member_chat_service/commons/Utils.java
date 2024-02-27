6e01b9255b53dd66c7976d591467a9dddba57075
SongHyunmsoo <y85858@naver.com>
2024-02-27 21:14:20 +0900

    코드 수정

--- src/main/java/Member_chat_service/commons/Utils.java
+++ src/main/java/Member_chat_service/commons/Utils.java
@@ -9,20 +9,27 @@ import org.springframework.validation.FieldError;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
 @Component
-@RequiredArgsConstructor
+@RequiredArgsConstructor// 빈 선언 의존성 주입을 위해
 public class Utils {
 
+    // 에러 코드에 대한 메시지 주입
     private final MessageSource messageSource;
 
 
+    // Validation에서 발생한 에러 메시지들을 Map 형태로 가져오는 메서드
     public Map<String, List<String>> getErrorMessages(Errors errors) {
         try {
+            // getFieldErrors 메서드로 각 필드에 발생한 에러 정보
+            // 각 FieldError에서 에러 메세지를 키 값 형태로 가져옴
             Map<String, List<String>> messages = errors.getFieldErrors()
                     .stream()
                     .collect(Collectors.toMap(FieldError::getField, e -> _getErrorMessages(e.getCodes()), (m1, m2) -> m2));
 
-
+            // getGlobalErrors 에러 정보를(ObjectError)를 가져옴
+            // ObjectError 에서 에러 코드를 추출 메세지
+            // 이 리스트를 "global"이라는 키로 맵에 추가합니다.
             List<String> gMessages = errors.getGlobalErrors()
                     .stream()
                     .map(o -> {
@@ -42,7 +49,7 @@ public class Utils {
         return null;
 
     }
-
+    // 주어진 에러 코드를 가져와 리스트로 반환  코드
     private List<String> _getErrorMessages(String[] codes) {
         List<String> messages = Arrays.stream(codes)
                 .map(c -> {
